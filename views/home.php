29271248a2ae6d760054911a79dbf69f566de749
NiclasNielsen <Niclasvnielsen@gmail.com>
2022-09-14 21:31:39 +0200

    Post and Comment display

--- views/home.php
+++ views/home.php
@@ -13,6 +13,15 @@
 
         foreach($pdo->query($postSql) as $post) {
     ?>
+    
+        <p>
+            <?php 
+                $posterId = $post['user_fk'];
+                $posterNameSql = "SELECT name FROM users WHERE user_id='$posterId'";
+                $posterName = $pdo->query($posterNameSql)->fetch();
+                echo $posterName[0];
+            ?>
+        </p>
         <h3>
             <?php echo $post['title']; ?>
         </h3>
@@ -29,40 +38,44 @@
 
                 foreach($pdo->query($commentSql) as $comment) {
             ?>
-                <p>
-                    <?php 
-                        $commenterId = $comment['user_fk'];
-                        $commenterNameSql = "SELECT name FROM users WHERE user_id='$commenterId'";
-                        $commenterName = $pdo->query($commenterNameSql)->fetch();
-                        echo $commenterName[0];
-                    ?>
-                </p>
-                <p>
-                    <?php echo $comment['comment'] ?>
-                </p>
-                
-                <div class="subComments">
-                    <?php 
-                        $comment_id = $comment['comment_id'];
-                        $subCommentSql = "SELECT * FROM comments WHERE fk_target = '2' AND fk = '$comment_id' ORDER BY date, likes DESC";
+                <div class="comment">
+                    <p>
+                        <?php 
+                            $commenterId = $comment['user_fk'];
+                            $commenterNameSql = "SELECT name FROM users WHERE user_id='$commenterId'";
+                            $commenterName = $pdo->query($commenterNameSql)->fetch();
+                            echo $commenterName[0];
+                        ?>
+                    </p>
+                    <p>
+                        <?php echo $comment['comment'] ?>
+                    </p>
+                    
+                    <div class="subComments">
+                        <?php 
+                            $comment_id = $comment['comment_id'];
+                            $subCommentSql = "SELECT * FROM comments WHERE fk_target = '2' AND fk = '$comment_id' ORDER BY date, likes DESC";
 
-                        foreach($pdo->query($subCommentSql) as $subComment) {
-                    ?>
-                        <p>
-                            <?php 
-                                $subCommenterId = $subComment['user_fk'];
-                                $subCommenterNameSql = "SELECT name FROM users WHERE user_id='$subCommenterId'";
-                                $subCommentName = $pdo->query($subCommenterNameSql)->fetch();
-                                echo $subCommentName[0];
-                            ?>
-                        </p>
-                        <p>
-                            <?php echo $subComment['comment'] ?>
-                        </p>
-                    <?php
-                        }
-                    ?>
-                </div> <!-- </Sub Comments> -->
+                            foreach($pdo->query($subCommentSql) as $subComment) {
+                        ?>
+                            <div class="subComment">
+                                <p>
+                                    <?php 
+                                        $subCommenterId = $subComment['user_fk'];
+                                        $subCommenterNameSql = "SELECT name FROM users WHERE user_id='$subCommenterId'";
+                                        $subCommentName = $pdo->query($subCommenterNameSql)->fetch();
+                                        echo $subCommentName[0];
+                                    ?>
+                                </p>
+                                <p>
+                                    <?php echo $subComment['comment'] ?>
+                                </p>
+                            </div>
+                        <?php
+                            }
+                        ?>
+                    </div> <!-- </Sub Comments> -->
+                </div>
             <?php
                 }
             ?>
