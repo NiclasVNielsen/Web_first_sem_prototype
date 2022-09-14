dbf55180e0d634e2ddd2af5b79bdee181897c012
NiclasNielsen <Niclasvnielsen@gmail.com>
2022-09-14 20:59:51 +0200

    post basic read

--- views/home.php
+++ views/home.php
@@ -1,8 +1,6 @@
 <?php
     /* things act like they are in the root folder here */
     require 'dbconn.php';
-
-    $sql = 'SELECT * FROM users'
 ?>
 
 <?php
@@ -11,15 +9,66 @@
 
 <div>
     <?php 
+        $postSql = 'SELECT * FROM posts';
 
-    foreach($pdo->query($sql) as $row) {
-        echo $row['name'] . "\n";
+        foreach($pdo->query($postSql) as $post) {
     ?>
-    <br>
-    <?php
-        echo $row['password'] . "\n";
-    }
+        <h3>
+            <?php echo $post['title']; ?>
+        </h3>
+        <p>
+            <?php echo $post['description']; ?>
+        </p>
+        <figure>
+            <img src="./src/uploads/<?php echo $post['image'] ?>" alt="<?php echo $post['title']; ?>">
+        </figure>
+        <div class="comments">
+            <?php 
+                $post_id = $post['post_id'];
+                $commentSql = "SELECT * FROM comments WHERE fk_target = '1' AND fk = '$post_id' ORDER BY likes, date DESC";
 
+                foreach($pdo->query($commentSql) as $comment) {
+            ?>
+                <p>
+                    <?php 
+                        $commenterId = $comment['user_fk'];
+                        $commenterNameSql = "SELECT name FROM users WHERE user_id='$commenterId'";
+                        $commenterName = $pdo->query($commenterNameSql)->fetch();
+                        echo $commenterName[0];
+                    ?>
+                </p>
+                <p>
+                    <?php echo $comment['comment'] ?>
+                </p>
+                
+                <div class="subComments">
+                    <?php 
+                        $comment_id = $comment['comment_id'];
+                        $subCommentSql = "SELECT * FROM comments WHERE fk_target = '2' AND fk = '$comment_id' ORDER BY date, likes DESC";
+
+                        foreach($pdo->query($subCommentSql) as $subComment) {
+                    ?>
+                        <p>
+                            <?php 
+                                $subCommenterId = $subComment['user_fk'];
+                                $subCommenterNameSql = "SELECT name FROM users WHERE user_id='$subCommenterId'";
+                                $subCommentName = $pdo->query($subCommenterNameSql)->fetch();
+                                echo $subCommentName[0];
+                            ?>
+                        </p>
+                        <p>
+                            <?php echo $subComment['comment'] ?>
+                        </p>
+                    <?php
+                        }
+                    ?>
+                </div> <!-- </Sub Comments> -->
+            <?php
+                }
+            ?>
+        </div> <!-- </Comments> -->
+    <?php
+        }
     ?>
 </div>
 
