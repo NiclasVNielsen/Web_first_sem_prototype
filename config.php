7a6eafc581d5bb91c182dc45cf835c09792b73e7
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2022-09-14 11:16:00 +0200

    Init with db conn and basic read

---
+++ config.php
@@ -0,0 +1,9 @@
+<!-- Tutorial https://www.phptutorial.net/php-pdo/pdo-connecting-to-mysql/ -->
+
+<!-- Plz make connection safer! -->
+<?php
+
+$host = 'localhost';
+$db = 'web_first_sem_prototype';
+$user = 'root';
+$password = '';
